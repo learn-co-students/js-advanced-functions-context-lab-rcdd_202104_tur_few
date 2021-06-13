9f03f5e36d074db27e9154d484ab81529091dd7b
Kutay Kagan Ozen <kutay.kagan.ozenn@gmail.com>
2021-06-13 21:57:51 +0300

    Done.

--- index.js
+++ index.js
@@ -1,13 +1,51 @@
 /* Your Code Here */
+function createEmployeeRecord (array) {
+    return {
+        firstName: array[0],
+        familyName: array[1],
+        title: array[2],
+        payPerHour: parseInt(array[3]),
+        timeInEvents: [],
+        timeOutEvents: []
+    }
+}
 
-/*
- We're giving you this function. Take a look at it, you might see some usage
- that's new and different. That's because we're avoiding a well-known, but
- sneaky bug that we'll cover in the next few lessons!
+function createEmployeeRecords(array) {
+    let records = []
+    array.forEach(element => {
+        records.push(createEmployeeRecord(element))
+    });
+    return records
+}
 
- As a result, the lessons for this function will pass *and* it will be available
- for you to use if you need it!
- */
+function createTimeInEvent (timeStamp) {
+    this.timeInEvents.push({
+        type: "TimeIn",
+        hour: parseInt(timeStamp.split(' ')[1]),
+        date: timeStamp.split(' ')[0]
+    })
+    return this
+}
+
+function createTimeOutEvent (timeStamp) {
+    this.timeOutEvents.push({
+        type: "TimeOut",
+        hour: parseInt(timeStamp.split(' ')[1]),
+        date: timeStamp.split(' ')[0]
+    })
+    return this
+}
+
+function hoursWorkedOnDate (date) {
+    let timeIn = this.timeInEvents.find(time => time.date == date)
+    let timeOut = this.timeOutEvents.find(time => time.date === date)
+
+    return (parseInt(timeOut.hour - timeIn.hour) / 100)
+}
+
+function wagesEarnedOnDate(date) {
+    return(hoursWorkedOnDate.call(this,date) * this.payPerHour)
+}
 
 let allWagesFor = function () {
     let eligibleDates = this.timeInEvents.map(function (e) {
@@ -20,3 +58,24 @@ let allWagesFor = function () {
 
     return payable
 }
+
+// function allWagesFor(employee) {
+//     const dates = employee.timeInEvents.map((e) => e.date)
+//     return dates.reduce((acc,curr) => (acc + wagesEarnedOnDate(employee, curr)), 0)
+// }
+
+function findEmployeeByFirstName(records, name) {
+    return records.find(employee => (employee.firstName === name))
+}
+
+function calculatePayroll(records) {
+    return records.reduce((acc, curr) => acc + allWagesFor.call(curr), 0)
+}
+/*
+ We're giving you this function. Take a look at it, you might see some usage
+ that's new and different. That's because we're avoiding a well-known, but
+ sneaky bug that we'll cover in the next few lessons!
+
+ As a result, the lessons for this function will pass *and* it will be available
+ for you to use if you need it!
+ */
